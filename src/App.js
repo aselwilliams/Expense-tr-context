d22227299076c1ac94d9e7c4f9c8b01338ba1a8b
aselwilliams <aselwilliams22@gmail.com>
2022-06-04 21:51:01 -0400

    app.js is done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
+import { Header } from "./components/Header";
+import { Balance } from "./components/Balance";
+import { IncomeExpenses } from "./components/IncomeExpenses";
+import { TransactionList } from "./components/TransactionList";
+import { AddTransaction } from "./components/AddTransaction";
 import './App.css';
 
+const localStorageTransactions=JSON.parse(localStorage.getItem('transactions'))
+
+const initialTransactions=localStorageTransactions || []
+
 function App() {
+  const [transactions,setTransactions] =useState(initialTransactions)
+
+  useEffect(()=>{
+    localStorage.setItem('transactions', JSON.stringify(transactions))
+  },[transactions]);
+
+  const addTransaction =(transaction)=>{
+    const newTransactions=transactions.map((t)=>{
+      return {...t}
+    })
+    newTransactions.push(transaction);
+    setTransactions(newTransactions);
+  };
+
+  const deleteTransaction=(id)=>{
+    const newTransactions=transactions.filter((t)=>t.id !== id);
+    setTransactions(newTransactions)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   <Header />
+   <div className="container">
+     <Balance transactions={transactions} />
+     <IncomeExpenses transactions={transactions} />
+     <TransactionList transactions={transactions} deleteTransaction={deleteTransaction} />
+     <AddTransaction addTransaction={addTransaction} />
+   </div>
+   </>
   );
 }
 
